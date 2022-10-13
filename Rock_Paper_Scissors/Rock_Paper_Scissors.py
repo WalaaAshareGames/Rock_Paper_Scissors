a9a43c27e9dec5c1c8106161cb82503fca6ab3bc
WalaaAtiah <walaaatiah97@gmail.com>
2022-10-13 21:21:56 +0000

    walaa1

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -1,14 +1,53 @@
 
-
+from game import game
 
 
 
 
 def Welcoming() :
     
-    print ("""Welcome to our Rock Paper Scissors game 
-    
-    lets start new round
+    print ("""
+     ###########################################################################
+                                                    
+                  Welcome to our Rock_Paper_Scissors game        
+          The principle of this game is that the rock breaks the scissors 
+          and the scissors cut the paper and the paper covers the room
+                 Whoever makes the strongest move wins the game
+                 rock > scissors, scissors > paper, paper > rock
+
+           This game requires two people and It consists of three rounds,
+                and whoever gets the highest total wins the game
+
+                Rock represent by "R"
+                paper represent by "P"
+                scissors represent by "S"
+
+                hint: you can play with computer or with your partner
+    ##############################################################################
     """)
-Act=input("enter your move")
+
+
+Welcoming()
+numper_player=input("if you want to play with computer enter 1 ,if you want to play with your partner enter 2 \n")
+
+game.set_player(numper_player)
+
+i=0
+total=0
+while i<3:
+   
+    char=input("enter you char (R:Rock ,P:Paper ,S:Scissors)")
+    total+=game.result(char)
+    print(total)
+    i +=1
+
+if total==0:
+    print("Draw")
+elif total>0:
+    print("Winner")
+else :
+    print("Loser")
+
+  
+
 
--- Rock_Paper_Scissors/game.py
+++ Rock_Paper_Scissors/game.py
@@ -2,7 +2,7 @@ import random
 
 
 
-class game :
+class Game :
     def __init__(self):
         self.player=0
 
@@ -12,9 +12,9 @@ class game :
     def get_player(self):
         return self.player
 
-    def game (self,char):
+    def result (self,char):
         count=0
-        if self.get_player()==1:
+        if self.get_player()=="1":
             list=['R','S','P']
             number= random.randint(0,2)
             v=list[number]
@@ -41,23 +41,17 @@ class game :
                 elif (char == "S"):
                     count=0
             return count
-                
-                
-                
 
 
+game =Game()   
 
-        
 
+if __name__ =="__main__" :
+    #test1
+    g1=game()
+    g1.set_player(1)
+    print(g1.get_player())
     
+    #test2
+    print(g1.game('R'))
     
-
-
-
-#test1
-g1=game()
-g1.set_player(1)
-print(g1.get_player())
-
-#test2
-print(g1.game('R'))
